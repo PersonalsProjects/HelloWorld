a2121a64d549fdd8c4fd8438224cf2f33cea1b19
chethanprakash <chethanssec@gmail.com>
2017-07-27 12:20:16 +0530

    Slave added

--- src/main/java/com/verify/Verification.java
+++ src/main/java/com/verify/Verification.java
@@ -5,6 +5,7 @@ public class Verification {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the world Chethan");
+		System.out.println("Slave adding");
 	}
 
 }
