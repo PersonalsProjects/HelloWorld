b198945b9091ba5581615b583423dd110877e4b0
chethanprakash <chethanssec@gmail.com>
2017-07-27 12:37:24 +0530

    Check added

---
+++ src/main/java/com/verify/Check.java
@@ -0,0 +1,11 @@
+package com.verify;
+
+public class Check {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the world Chethan");
+		System.out.println("Master check changed");
+	}
+
+}
