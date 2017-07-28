ea49e03d49f56fbed19b1682af6184915f801285
chethanprakash <chethanssec@gmail.com>
2017-07-28 10:07:20 +0530

    aDDING Master

--- src/main/java/com/verify/Check.java
+++ src/main/java/com/verify/Check.java
@@ -4,7 +4,7 @@ public class Check {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to the world Chethan");
+		System.out.println("Welcome to the world Chethan THE MASTER");
 		System.out.println("Master check changed");
 	}
 
